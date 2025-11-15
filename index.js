ccaea32b06065f9406dc0a5400d0769bd2530015
tushargangarde28-eng <tushargangarde28@gmail.com>
2025-11-15 19:28:55 +0530

    launch

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var session = require("express-session");
 var upload = require("express-fileupload");
-
-var MySQLStore = require("express-mysql-session")(session);
+var MySQLStore = require("express-mysql-session")(session);   // ⭐ Added
 
 var common_route = require("./routes/common");
 var admin_route = require("./routes/admin");
@@ -13,12 +12,12 @@ var employee_route = require("./routes/employee");
 require("dotenv").config();
 
 var app = express();
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(upload());
-app.use(express.static("public/"))
+app.use(express.static("public/"));
 
 
-var sessionStore = new MySQLStore({
+const sessionStore = new MySQLStore({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
@@ -26,15 +25,16 @@ var sessionStore = new MySQLStore({
 });
 
 app.use(session({
-    secret:"tushar",
-    resave:false,
-    saveUninitialized:false,
-    store: sessionStore  
+    secret: "tushar",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore                
 }));
 
-app.use("/",common_route);
-app.use("/employee",employee_route);
-app.use("/company",company_route);
-app.use("/admin",admin_route);
+
+app.use("/", common_route);
+app.use("/employee", employee_route);
+app.use("/company", company_route);
+app.use("/admin", admin_route);
 
 app.listen(process.env.PORT);
